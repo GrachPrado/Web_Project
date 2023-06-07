cba34cd9fd56d1d852baa95134a2c9b47b7114ec
Bogdan <bgra4ov@gmail.com>
2023-06-07 15:24:52 +0300

    mob header finished

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 function openNav() {
-    document.getElementById("header__menu").style.width = "100vw";
+    document.getElementById("header__menu").style.width = "110vw";
 
 }
 function closeNav(){
   document.getElementById("header__menu").style.width = "0";
 }
+
+document.getElementById("closeMenu").addEventListener("click", closeNav);
+document.getElementById("openNav").addEventListener("click", openNav);
