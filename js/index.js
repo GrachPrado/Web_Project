985e5babc876da58d58c8595ccb9717e0894ee83
Bogdan <bgra4ov@gmail.com>
2023-06-29 14:09:56 +0300

    JS problem solved

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-
-  function openNav() {
-    document.querySelector("#header__menu").style.width = "100vw";
-
-}
-function closeNav(){
-  document.querySelector("#header__menu").style.width = "0";
-}
-
-  document.querySelector("#closeMenu").addEventListener("click", closeNav);
-  document.querySelector("#openNav").addEventListener("click", openNav);
+document.addEventListener('partialsLoaded', () => {
+    import('./navigation.js');
+});
